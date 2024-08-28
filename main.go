502edff669302b8fb84e29ee18f11f8bf6637af4
kai <nerdrun.kai@gmail.com>
2024-08-28 18:23:32 +0200

    refactoring

--- main.go
+++ main.go
@@ -26,81 +26,44 @@ func load() int {
 	return total
 }
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
+func prompt(total int) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (1 - Input, 2 - Output, 3 - Save, 4 - Exit): ", reader)
+
+	switch opt {
+	case "1":
+		input, _ := getInput("Your input: ", reader)
+		added, _ := strconv.Atoi(input)
+		total += added
+		fmt.Printf("Total : %d\n", total)
+		prompt(total)
+	case "2":
+		output, _ := getInput("Your output : ", reader)
+		subtracted, _ := strconv.Atoi(output)
+		total -= subtracted
+		fmt.Printf("Total : %d\n", total)
+		prompt(total)
+	case "3":
+		err := os.WriteFile("./total.txt", []byte(strconv.Itoa(total)), fs.ModePerm)
+		if err != nil {
+			fmt.Printf("Writing file failed : %v", err)
+		}
+		fmt.Println("Your file is saved successfully.")
+	case "4":
+		fmt.Println("Thank you")
+	default:
+		fmt.Println("Nothing in the list")
+		prompt(total)
+	}
+}
+
 func main() {
 	fmt.Println("Welcome, this is an account book in terminal")
-	total := load()
-
-	keepGoing := true
-
-	for keepGoing {
-		reader := bufio.NewReader(os.Stdin)
-		displayMainMenu()
-
-		command, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
-
-		switch strings.TrimSuffix(command, "\n") {
-		case "1":
-			keepGoing := true
-			for keepGoing {
-				fmt.Println("How much do you want to add?")
-				command, err := reader.ReadString('\n')
-				if err != nil {
-					panic(err)
-				}
-				newInput := strings.TrimSuffix(command, "\n")
-				added, _ := strconv.Atoi(newInput)
-				total += added
-				fmt.Printf("total : %d \n\n", total)
-
-				keepGoing = toContinue("Do you want to add more?", reader)
-			}
-		case "2":
-			keepGoing := true
-			for keepGoing {
-				fmt.Println("How much do you want to subtract?")
-				command, err := reader.ReadString('\n')
-				if err != nil {
-					panic(err)
-				}
-				newInput := strings.TrimSuffix(command, "\n")
-				subtracted, _ := strconv.Atoi(newInput)
-				total -= subtracted
-				fmt.Printf("total : %d \n\n", total)
-
-				keepGoing = toContinue("Do you want to subtract more?", reader)
-			}
-		case "3":
-			err := os.WriteFile("./total.txt", []byte(strconv.Itoa(total)), fs.ModePerm)
-			if err != nil {
-				fmt.Printf("Writing file failed : %v", err)
-			}
-			fmt.Println("You selected 3")
-		case "4":
-			fmt.Println("You selected 4")
-			keepGoing = false
-		default:
-			fmt.Println("Nothing in the list")
-		}
-	}
-	fmt.Println("Thank you")
-}
-
-func displayMainMenu() {
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Add input")
-	fmt.Println("2. Add output")
-	fmt.Println("3. Save")
-	fmt.Println("4. Exit")
-	fmt.Print("Select: ")
-}
-
-func toContinue(title string, reader *bufio.Reader) bool {
-	fmt.Println(title)
-	fmt.Println("1. Yes")
-	fmt.Println("2. No")
-	command, _ := reader.ReadString('\n')
-	return strings.TrimSuffix(command, "\n") != "2"
+	prompt(load())
 }
